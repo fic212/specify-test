846ea8615550bcde8d6895ab82d617fd33d97bf3
Filip Debelić <filip.debelic@superology.com>
2023-01-11 10:06:23 +0100

    Update script for both light and dart theme.

--- generate_all.kts
+++ generate_all.kts
@@ -2,42 +2,75 @@
 
 import java.io.File
 
-val input = File("output/tokens/color/base.json")
-val colorsMap = input
-    .readText()
-    .trimIndent()
-    .replace("\n", "")
-    .replace(" ", "")
-    .removeSurrounding(prefix = "{\"color\":{\"base\":{", suffix = "}}}")
-    .split(",")
-    .associate {
-        val keyAndValue = it.split(":")
-        val key = keyAndValue.first().removeSurrounding("\"")
-        val value = keyAndValue.last().removeSurrounding(prefix = "\"", suffix = "\"}")
-        key to value
-    }
+private val RESOURCES_START = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<resources>"
+private val RESOURCES_END = "\n</resources>"
 
-val colorsOutputTextBuilder = StringBuilder()
-colorsOutputTextBuilder.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<resources>")
-val attrsOutputTextBuilder = StringBuilder()
-attrsOutputTextBuilder.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<resources>")
-val themeOutputTextBuilder = StringBuilder()
-themeOutputTextBuilder.append(
-    "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<resources>\n" +
-            "    <style name=\"Theme.Superbet.CoreUi.Light\" parent=\"Theme.MaterialComponents.NoActionBar\">"
-)
+val lightColorsMap = File("output/light/color/base.json").getColors()
+val darkColorsMap = File("output/dark/color/base.json").getColors()
 
-colorsMap.filterKeys {
-    !it.startsWith("base")
-}.toSortedMap().forEach { (key, value) ->
-    colorsOutputTextBuilder.append("\n    <color name=\"${key}Light\">$value</color>")
-    attrsOutputTextBuilder.append("\n    <attr name=\"$key\" format=\"color\" />")
-    themeOutputTextBuilder.append("\n        <item name=\"$key\">@color/${key}Light</item>")
+val attrsTextBuilder = StringBuilder(RESOURCES_START)
+val lightColorsTextBuilder = StringBuilder(RESOURCES_START)
+val darkColorsTextBuilder = StringBuilder(RESOURCES_START)
+val lightThemeTextBuilder = StringBuilder(RESOURCES_START)
+    .append(
+        "    <style name=\"Theme.Superbet.CoreUi.Light\" parent=\"Theme.MaterialComponents.NoActionBar\">"
+    )
+val darkThemeTextBuilder = StringBuilder(RESOURCES_START)
+    .append(
+        "    <style name=\"Theme.Superbet.CoreUi.Dark\" parent=\"Theme.MaterialComponents.NoActionBar\">"
+    )
+
+val attrsSet = HashSet<String>()
+// Append all light colors to colors and theme string builder.
+lightColorsMap.forEach { (key, value) ->
+    attrsSet.add(key)
+    lightColorsTextBuilder.append("\n    <color name=\"${key}Light\">${value}</color>")
+    lightThemeTextBuilder.append("\n        <item name=\"$key\">@color/${key}Light</item>")
 }
 
-colorsOutputTextBuilder.append("\n</resources>")
-attrsOutputTextBuilder.append("\n</resources>")
-themeOutputTextBuilder.append("\n    </style>\n</resources>")
-File("attrs.xml").writeText(attrsOutputTextBuilder.toString())
-File("colors_light.xml").writeText(colorsOutputTextBuilder.toString())
-File("theme_light.xml").writeText(themeOutputTextBuilder.toString())
+// Append all dark colors to colors and theme string builder.
+darkColorsMap.forEach { (key, value) ->
+    attrsSet.add(key)
+    darkColorsTextBuilder.append("\n    <color name=\"${key}Dark\">${value}</color>")
+    darkThemeTextBuilder.append("\n        <item name=\"$key\">@color/${key}Dark</item>")
+}
+
+// Append all attrs to attrs string builder.
+attrsSet.forEach {
+    attrsTextBuilder.append("\n    <attr name=\"$it\" format=\"color\" />")
+}
+
+// Append end tags to all string builders
+attrsTextBuilder.append(RESOURCES_END)
+lightColorsTextBuilder.append(RESOURCES_END)
+darkColorsTextBuilder.append(RESOURCES_END)
+lightThemeTextBuilder.append("\n    </style>").append(RESOURCES_END)
+darkThemeTextBuilder.append("\n    </style>").append(RESOURCES_END)
+
+// Write all strings builders to files.
+val resPath = "src/main/res/values"
+File("${resPath}/colors_attrs.xml").writeText(attrsTextBuilder.toString())
+File("${resPath}/colors_light.xml").writeText(lightColorsTextBuilder.toString())
+File("${resPath}/colors_dark.xml").writeText(darkColorsMap.toString())
+File("${resPath}/theme_light.xml").writeText(lightThemeTextBuilder.toString())
+File("${resPath}/theme_dark.xml").writeText(darkThemeTextBuilder.toString())
+
+/**
+ * Parses receiver json file and return all the colors as key value map.
+ */
+fun File.getColors(): Map<String, String> {
+    return readText()
+        .trimIndent()
+        .replace(oldValue = "\n", newValue = "")
+        .replace(oldValue = " ", newValue = "")
+        .removeSurrounding(prefix = "{\"color\":{\"base\":{", suffix = "}}}")
+        .split(",")
+        .associate {
+            val keyAndValue = it.split(":")
+            val key = keyAndValue.first().removeSurrounding("\"")
+            val value = keyAndValue.last().removeSurrounding(prefix = "\"", suffix = "\"}")
+            key to value
+        }.filterKeys {
+            !it.startsWith("base")
+        }
+}
